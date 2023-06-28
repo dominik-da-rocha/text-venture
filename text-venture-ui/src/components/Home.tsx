13b73645228c09653ea9f71976b489006dc269c8
Rocha <dominik.da.rocha@gmail.com>
2023-06-29 00:24:36 +0200

    - edited about page

--- text-venture-ui/src/components/Home.tsx
+++ text-venture-ui/src/components/Home.tsx
@@ -54,14 +54,48 @@ export function Home(props: HomeProps) {
 
 export function About() {
   return (
-    <div>
-      <h1>Text Venture</h1>
-      <div className="Subtitle">About the the Website</div>
+    <div className="About">
+      <h1>About Text Venture</h1>
+
+      <h3>Legal</h3>
       <p>
-        <h3>Contact</h3>
-        <div>Dominik da Rocha</div>
-        <div>dominik.da.rocha@gmail.com</div>
+        Text Venture is a fictional website created for demonstration purposes
+        only. Any resemblance to real websites or products is purely
+        coincidental.
       </p>
+
+      <h3>Issues</h3>
+      <p>
+        If you encounter any issues or have feature requests for Text Venture,
+        please visit the GitHub repository:
+      </p>
+      <p>
+        <a
+          className="github-link"
+          href="https://github.com/dominik-da-rocha/text-venture"
+        >
+          <img
+            src={"/icons/github.svg"}
+            alt="https://github.com/dominik-da-rocha/text-venture"
+          />
+          <span>https://github.com/dominik-da-rocha/text-venture</span>
+        </a>
+      </p>
+
+      <h3>Acknowledgements</h3>
+      <ul></ul>
+
+      <h3>Impressum</h3>
+      <p>
+        <span>
+          Dominik da Rocha <br />
+        </span>
+        <span>
+          todo-get-an-email@text-venture.com
+          <br />
+        </span>
+      </p>
+
       <p>
         <Link to="/">Back to Home</Link>
       </p>
