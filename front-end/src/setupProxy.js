a6a01dc806b3074ce1b31173788aafcdaf99e423
Rocha <dominik.da.rocha@gmail.com>
2023-07-03 19:27:39 +0200

    changed default port of backend

--- front-end/src/setupProxy.js
+++ front-end/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api/v1",
     createProxyMiddleware({
-      target: "http://localhost:7080",
+      target: "http://localhost:8081",
       changeOrigin: true,
     })
   );
